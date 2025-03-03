f37b655bc9bbe2e4dfbaae630c75effacae981cf
Terence <terence9213@gmail.com>
2025-03-03 22:08:33 +0800

    First Commit
    Create solution RadioactivityMonitor

---
+++ Reactor/Alarm.cs
@@ -0,0 +1,30 @@
+﻿namespace RadioactivityMonitor
+{
+    public class Alarm
+    {
+        private const double LowThreshold = 17;
+        private const double HighThreshold = 21;
+
+        Sensor _sensor = new Sensor();
+
+        bool _alarmOn = false;
+        private long _alarmCount = 0;
+
+
+        public void Check()
+        {
+            double value = _sensor.NextMeasure();
+
+            if (value < LowThreshold | HighThreshold < value)
+            {
+                _alarmOn = true;
+                _alarmCount += 1;
+            }
+        }
+
+        public bool AlarmOn
+        {
+            get { return _alarmOn; }
+        }
+    }
+}
---
+++ ReactorAlarmTests/AlarmTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RadioactivityMonitor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioactivityMonitor.Tests
+{
+    [TestClass()]
+    public class AlarmTests
+    {
+        [TestMethod()]
+        public void CheckTest()
+        {
+            Alarm alarm = new Alarm();
+            alarm.Check();
+
+            Assert.IsTrue(alarm.AlarmOn);
+
+        }
+    }
+}
